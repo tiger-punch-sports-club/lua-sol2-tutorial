60b39fdeed926ae5a6150205a9f486b66d01d8c8
Sergej Schefer <sergej.schefer@web.de>
2020-11-22 11:23:45 +0100

    tutorial part 2

--- source/main.cpp
+++ source/main.cpp
@@ -1,11 +1,100 @@
-// #include <lua/lua.hpp>
-// #include <sol/sol.hpp>
-
+#include <lua/lua.hpp>
+#include <sol/sol.hpp>
 #include <stdio.h>
 
-void main() {
-	printf("Hello World\n");
+void RandomStuff()
+{
+    printf("RandomStuff()\n");
+}
 
-	// sol::state lua{}; 
+int GetRandomNumber(int value)
+{
+    return value;
+}
 
+struct MyStruct
+{
+    float x, y;
+
+    MyStruct() : x(0), y(0)
+    {
+    }
+
+    MyStruct(float x, float y) : x(x), y(y)
+    {
+    }
+
+    void Print() const
+    {
+        printf("%f, %f\n", x, y);
+    }
+
+    float GetX() const
+    {
+        return x;
+    }
+
+    float GetY() const
+    {
+        return y;
+    }
+
+    void SetX(const float& x)
+    {
+        this->x = x;
+    }
+
+    void SetY(const float& y)
+    {
+        this->y = y;
+    }
+};
+
+void DoStuffToStruct(MyStruct& myStruct)
+{
+    myStruct.x = 200;
+    myStruct.y = 200;
+}
+
+void main() 
+{
+    sol::state lua{};
+    lua.open_libraries(sol::lib::base);
+
+    // 1. Run Lua scripts
+    lua.script("print('Hello World from Lua!')");
+    
+    // 3. Bind and call C++ functions to Lua
+    lua.set_function("RandomStuff", &RandomStuff);
+    lua.set_function("GetRandomNumber", &GetRandomNumber);
+    lua.set_function("DoStuffToStruct", &DoStuffToStruct);
+
+    
+    // 4. Bind and use C++ data to Lua
+    lua.new_usertype<MyStruct>("MyStruct",
+        sol::constructors<void(), void(float, float)>(),
+        "x", &MyStruct::x,
+        "y", &MyStruct::y,
+        "Print", &MyStruct::Print,
+        "GetX", &MyStruct::GetX,
+        "GetY", &MyStruct::GetY,
+        "SetX", &MyStruct::SetX,
+        "SetY", &MyStruct::SetY
+    );
+        
+    // 2. Load Lua scripts from disk
+    lua.script_file("script.lua");
+    
+    // 5. Call Lua functions from C++ (With return types)
+    const std::function<void()>& luaFunction = lua["LuaFunction"];
+    luaFunction();
+
+    const std::function<int(int)>& luaFunction2 = lua["LuaFunction2"];
+    auto value = luaFunction2(123);
+    printf("value = %d \n", value);
+
+    MyStruct myStruct;
+    const std::function<void(MyStruct&)>& luaFunction3 = lua["LuaFunction3"];
+    luaFunction3(myStruct);
+    myStruct.Print();
 }
---
+++ source/script.lua
@@ -0,0 +1,41 @@
+﻿print("Script.lua loaded!")
+
+RandomStuff()
+
+local Value = GetRandomNumber(123)
+print(Value)
+
+local struct1 = MyStruct:new()
+local struct2 = MyStruct:new(1, 2)
+
+print(struct1.x .. " x " .. struct1.y)
+print(struct2.x .. " x " .. struct2.y)
+
+struct1.x = 12
+struct1.y = 14
+
+print(struct1.x .. " x " .. struct1.y)
+
+struct1:Print()
+struct1:SetX(100)
+struct1:Print()
+
+local x = struct1:GetX()
+print("X = " .. x)
+
+DoStuffToStruct(struct2)
+struct2:Print()
+
+function LuaFunction()
+    print("LuaFunction() called")
+end
+
+function LuaFunction2(x)
+    print(x)
+    return x
+end
+
+function LuaFunction3(myStruct)
+    myStruct.x = 111
+    myStruct.y = 222
+end
